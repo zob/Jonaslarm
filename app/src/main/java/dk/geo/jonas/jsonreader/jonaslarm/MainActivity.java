20b9bb64b9c48c04dae0f5ef605e30bb9771418e
zob <larszobbe@gmail.com>
2015-07-11 15:56:15 +0200

    First commit

---
+++ app/src/main/java/dk/geo/jonas/jsonreader/jonaslarm/MainActivity.java
@@ -0,0 +1,67 @@
+package dk.geo.jonas.jsonreader.jonaslarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * This sample demonstrates how to schedule an alarm that causes a service to
+ * be started. This is useful when you want to schedule alarms that initiate
+ * long-running operations, such as retrieving a daily forecast.
+ * This particular sample retrieves content from the Google home page once a day and
+ * checks it for the search string "doodle". If it finds this string, that indicates
+ * that the page contains a custom doodle instead of the standard Google logo.
+ */
+public class MainActivity extends Activity {
+
+    SampleAlarmReceiver alarm = new SampleAlarmReceiver();
+    Button startButton;
+    Button cancelButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.start_button);
+        cancelButton = (Button) findViewById(R.id.cancel_button);
+
+
+    }
+
+    public void onStartClick(View view) {
+        alarm.setAlarm(this);
+        Toast.makeText(this, "start clicked", Toast.LENGTH_LONG).show();
+    }
+
+    public void onCancelClick(View view) {
+        alarm.cancelAlarm(this);
+        Toast.makeText(this, "cancel clicked", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //When the user clicks START ALARM, set the alarm
+            case R.id.start_action:
+                alarm.setAlarm(this);
+                return true;
+            // When the user clicks CANCEL ALARM, cancel the alarm.
+            case R.id.cancel_action:
+                alarm.cancelAlarm(this);
+                return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/dk/geo/jonas/jsonreader/jonaslarm/ServerPollService.java
@@ -0,0 +1,41 @@
+package dk.geo.jonas.jsonreader.jonaslarm;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+/**
+ * This {@code IntentService} does the app's actual work.
+ * {@code SampleAlarmReceiver} (a {@code WakefulBroadcastReceiver}) holds a
+ * partial wake lock for this service while the service does its work. When the
+ * service is finished, it calls {@code completeWakefulIntent()} to release the
+ * wake lock.
+ */
+public class ServerPollService extends IntentService {
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public ServerPollService() {
+        super("SchedulingService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
